125400a80e1c4d8d90fb35d14b7b96159cc5f1f0
Ryan Cheek <ryan.cheek@bettercloud.com>
2015-10-19 11:52:28 -0400

    Update

---
+++ src/test/groovy/CoverageSuite/CoverageSuiteTemplate.groovy
@@ -0,0 +1,14 @@
+package CoverageSuite
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+/**
+ * Created by Ryan on 10/19/2015.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses([
+
+])
+class CoverageSuiteTemplate {
+}

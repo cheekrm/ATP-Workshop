220b9eeff8756fd753bf01e8d7613ea3f9d89d27
Ryan Cheek <ryan.cheek@bettercloud.com>
2015-10-19 13:42:54 -0400

    Update

--- src/test/groovy/CoverageSuite/CoverageSuiteTemplate.groovy
+++ src/test/groovy/CoverageSuite/CoverageSuiteTemplate.groovy
@@ -1,5 +1,6 @@
 package CoverageSuite
 
+
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -7,9 +7,22 @@ import org.openqa.selenium.chrome.ChromeDriver
 
 driver =
 {
-    System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\Drivers\\chromedriver\\chromedriver.exe");
+    String osName = System.getProperty("os.name").toLowerCase()
+    if (osName.contains("windows")) {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\Drivers\\chromedriver\\chromedriver.exe");
+    } else if (osName.contains("mac")) {
+        return OperatingSystem.OSX
+    } else if (osName.contains("linux")) {
+        return OperatingSystem.LINUX
+    } else {
+        throw new Exception("Unrecognized operating system '${osName}'.")
+    }
+
     WebDriver driver = new ChromeDriver()
     driver
 }
 
 reportsDir = "C:\\"
+baseUrl = ""
+atCheckWaiting = [30, 1]
+autoClearCookies = false
